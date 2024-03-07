30e6fd5418211c14d7704d1c2cbb1ad37480e90c
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2024-03-07 09:01:34 +0100

    updated requirements

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-93k8tl70own-lzramq#t^0s+i*rx!ue$q_cmvhos-kr@*cq#3_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Wilhelm.pythonanywhere.com']
+ALLOWED_HOSTS = ['Wilhelm.pythonanywhere.com',
+                 "127.0.0.1"]
 
 
 # Application definition
